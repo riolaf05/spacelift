b54d479a25cdf53f99890db51464dd5087410ea3
Rosario Laface <lafacerosario@gmail.com>
2024-07-09 13:44:41 +0200

    init

--- s3.tf
+++ s3.tf
@@ -1,21 +1,8 @@
-data "aws_caller_identity" "current" {}
+resource "aws_s3_bucket" "example" {
+  bucket = "rio-spaceshift-demo"
 
-module "s3" {
-  source = "git::https://github.com/riolaf05/terraform-modules//aws/s3"
-
-  project_info = {
-    name   = "rio"
-    prefix = "rio"
-    env    = "dev"
+  tags = {
+    Name        = "PROJECT"
+    Environment = "POC"
   }
-
-  bucket_name      = "spacelift-bucket"
-  ssm_param_prefix = "rio"
-  bucket_user_arn   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/rosario.laface"
 }
-
-resource "aws_s3_object" "object_raw" {
-  bucket = module.s3.bucket_name
-  key    = "cartella/"
-}
-
